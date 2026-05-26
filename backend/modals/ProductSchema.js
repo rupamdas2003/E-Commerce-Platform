77928372a84fcee181d92181b018fce96f791831
RUPAM DAS <132121805+rupamdas2003@users.noreply.github.com>
2026-05-26 20:37:47 +0530

    Add files via upload

---
+++ backend/modals/ProductSchema.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter product name"],
+    },
+    description: {
+        type: String,
+        required: [true, "Please Enter product description"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please Enter product price"],
+        maxLength: [8, "Please cannot exceed 8 character"],
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    images: [
+        // id jha pe image ko upload karenge
+        {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [8, "Please Enter product category"],
+    },
+    Stock: {
+        type: Number,
+        required: [8, "Please Enter product category"],
+        maxLength: [4, "Stock cannot exceed 8 character"],
+        default: 1,
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0,
+    },
+    reviews: [
+        {
+            userRevId: {
+                type: mongoose.Schema.ObjectId,
+                ref: "user",
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: Number,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.models.product || mongoose.model("product", ProductSchema);
